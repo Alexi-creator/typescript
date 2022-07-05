365ca2e07619c465fdb4abf375954113c67a1730
ilya Pavlov <ilya.p@finanso.com>
2022-07-05 19:39:46 +0300

    add ts

---
+++ app.ts
@@ -0,0 +1,10 @@
+let revenue: number = 1000;
+let bonus: number = 500;
+
+let res: number = revenue + bonus;
+console.log(res);
+
+
+function getFullName(firstName: string, surname: string): string {
+    return `${firstName} ${surname}`;
+}
